369f82e0ebfda3ab815b7ced5f4a845b72634f7b
Bahadir Durmaz <bahadiracimaz@gmail.com>
2022-04-27 00:53:59 +0300

    integration test script complete

--- storylyapi/stories/tests.py
+++ storylyapi/stories/tests.py
@@ -1,95 +1,72 @@
-from django.test import Client
-import unittest
+import json
+from django.test import TestCase, Client
+from .models import Story, App, Metadata, Event
+from datetime import datetime
 
-class TestStories(unittest.TestCase):
+class TestStories(TestCase):
     def setUp(self):
+        Metadata.objects.create(metadata="{'test': 'test'}", story_id=1)
+        App.objects.create(id=1)
+        Story.objects.create(app_id=1, ts=datetime(2020,4,19,12,0,0))
+
+        Event.objects.create(
+            event_type='test',
+            story_id=1,
+            app_id=1,
+            count=1,
+            date=datetime.today()
+        )
         self.client = Client()
 
-        #setup database response
-        # https://docs.djangoproject.com/en/4.0/topics/testing/overview/
-
     def test_get_stories(self):
-        response = self.client.get('/stories/1')
+        response = self.client.get('/api/v1/stories/1')
         self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json()
+            .get('metadata'), [{'id': 1, 'metadata': "{'test': 'test'}"}])
+        self.assertEqual(response.json()
+            .get('ts'), 1587297600)
+
 
     def test_get_stories_not_found(self):
-        response = self.client.get('/stories/2')
+        response = self.client.get('/api/v1/stories/2')
         self.assertEqual(response.status_code, 404)
 
     def test_get_stories_cached(self):
-        response = self.client.get('/stories/cached/1')
+        response = self.client.get('/api/v1/stories/cached/1')
         self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json()
+            .get('metadata'), [{'id': 1, 'metadata': "{'test': 'test'}"}])
+        self.assertEqual(response.json()
+            .get('ts'), 1587297600)
 
     def test_get_stories_cached_not_found(self):
-        response = self.client.get('/stories/cached/2')
+        response = self.client.get('/api/v1/stories/cached/2')
         self.assertEqual(response.status_code, 404)
 
     def test_create_event(self):
-        response = self.client.post('/event/1', {'event_type': 'test', 'story': '1'})
-        self.assertEqual(response.status_code, 201)
+        response = self.client.post('/api/v1/event/1', data=json.dumps({
+            "event_type":'test',
+            "user_id":1,
+            "story_id":1,
+        }), content_type="application/json")
 
-    def test_create_event_not_found(self):
-        response = self.client.post('/event/2', {'event_type': 'test', 'story': '1'})
-        self.assertEqual(response.status_code, 404)
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(response.json()
+            .get('event_type'), 'test')
+        self.assertEqual(response.json()
+            .get('story'), 1)
+        self.assertEqual(response.json()
+            .get('count'), 2)
+        self.assertEqual(response.json()
+            .get('date'), datetime.today().strftime('%Y-%m-%d'))
+
 
     def test_create_event_not_found_story(self):
-        response = self.client.post('/event/1', {'event_type': 'test', 'story': '2'})
+        response = self.client.post('/event/2', data=json.dumps({
+            "event_type":'test',
+            "user_id":1,
+            "story_id":1,
+        }), content_type="application/json")
+
         self.assertEqual(response.status_code, 404)
-
-    def test_create_event_not_found_event_type(self):
-        response = self.client.post('/event/1', {'event_type': 'test2', 'story': '1'})
-        self.assertEqual(response.status_code, 404)
-
-    #def test_create_event_not_found_event_type_story(self):
-    #    response = self.client.post('/event/
-
-
-
-# test for create event
-#https://www.youtube.com/watch?v=IKnp2ckuhzg
-
-#test models 
     
-#https://www.youtube.com/watch?v=zUl-Tf-UEzw schema test
-
-
-
-"""
-@lgabs you can make use of unittest
-For example:
-
-# Create your tests here.
-import unittest
-from django.test import Client
-
-class MyUnitTest(unittest.TestCase):
-    def setUp(self):
-        # Every test needs a client.
-        self.client = Client()
-
-    def test_product_health(self):
-        # Issue a GET request.
-        response = self.client.get('/api/v1/products/ping')
-        # Check that the response is 200 OK.
-        self.assertEqual(response.status_code, 200)
-        # Check that the rendered json contains valid data.
-        self.assertEqual(response.json().get('data'), 'Healthy')
-
-$ ./manage.py test APP_NAME
-
-Since it's under Django so you can also leverage Django default unit test.
-
-from django.test import TestCase
-class MyUnitTest(TestCase):
-    pass
-
-"""
-
-
-#unittest apis
-
-
-#RenewBookForm schema testi https://developer.mozilla.org/en-US/docs/Learn/Server-side/Django/Testing
-
-
-#make request https://www.rootstrap.com/blog/testing-in-django-django-rest-basics-useful-tools-good-practices/ orta sonlar
